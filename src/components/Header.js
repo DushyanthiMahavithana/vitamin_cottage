018aa60463fba2b10e75b08750f2fccecfa73369
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-15 22:24:46 +0530

    register page created

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,16 @@ export default function Header() {
   return (
     <header className='flex items-center justify-between'>
       <Link className='text-4xl text-green-500 py-2 font-mono' href={'/'}>VITAMIN COTTAGE</Link>
-      <nav className='flex items-center gap-10 font-semibold font-mono'>
+      <nav className='flex items-center gap-5 font-semibold font-mono'>
         <Link className=' hover:text-gray-400' href={'/'}>Home</Link>
         <Link className=' hover:text-gray-400' href={'/menu'}>Menu</Link>
         <Link className=' hover:text-gray-400' href={'/about'}>About Us</Link>
         <Link className=' hover:text-gray-400' href={'/contact'}>Contact</Link>
-        <Link className='bg-green-500 px-8 py-2 rounded-lg hover:text-gray-400 hover:bg-green-600' href={'/login'}>LOGIN</Link>
+        
+      </nav>
+      <nav className='flex items-center gap-4 font-semibold font-mono'>
+        <Link className=' hover:text-gray-400' href={'/login'}>login</Link>
+        <Link className='bg-green-500 px-8 py-2 rounded-lg hover:text-gray-400 hover:bg-green-600' href={'/register'}>REGISTER</Link>
       </nav>
     </header>
   )
