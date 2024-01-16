1fdabe74d695ba4b638bc478b00866940045b95f
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-16 11:44:16 +0530

    installed shad/ui

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import SectionHeaders from '@/components/SectionHeaders'
 
 export default function Registerpage() {
   return (
-    <div>
-      redsds
+    <div className='text-center mt-20'>
+      <SectionHeaders mainheader={'Register Now'}></SectionHeaders>
+      
     </div>
   )
 }
